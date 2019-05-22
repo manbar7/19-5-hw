c3e1767adbfa6925bf4770e2be79e812740a05ff
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-05-22 17:05:17 +0300

    Add files via upload

---
+++ ConsoleApp38/AtomicCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    class AtomicCalculator
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Func<int> GetNumberFromUser { get; set; }
+        public Action MenuPrinter { get; set; }
+        public Func<int> GetUserChoice { get; set; }
+        public Func<int,int,double> Calculate { get; set; }
+        public Action<double> ResultPrinter { get; set; }
+
+
+    }
+}
